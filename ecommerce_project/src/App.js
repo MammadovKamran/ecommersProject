17ea44a4d9fccc928cadc174990cc505d84b3ce8
vusaldurmushov <vusal.durmushov06@gmail.com>
2022-09-25 16:21:20 +0400

    navbar-design

--- ecommerce_project/src/App.js
+++ ecommerce_project/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Header from './components/Header/Header';
-
+import Home from "./pages/Home/Home"
 function App() {
   return (
     <>
-
-<Header/>
+      <Header />
+      <Home />
     </>
   );
 }
