dafd0bfc870e50f80a04f5980f933ced937d76b0
vusaldurmushov <vusal.durmushov06@gmail.com>
2022-09-26 18:12:41 +0400

    nese

--- ecommerce_project/src/root/App.js
+++ ecommerce_project/src/root/App.js
@@ -3,7 +3,6 @@ import { useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Dashboard from "./Dashboard";
-import Home from "../pages/Home/Home";
 import "../App.css"
 
 const App = () => {
@@ -13,7 +12,7 @@ const App = () => {
       {pathname !== "/signUp" && pathname !== "/signIn" ? <Header /> : <div />}
       <Dashboard />
       {pathname !== "/signUp" && pathname !== "/signIn" ? <Footer /> : <div />}
-    <Home/>
+ 
     </div>
   );
 };
