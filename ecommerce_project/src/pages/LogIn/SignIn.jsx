c41f8ef1e32f57089c127729aac54c7472195654
MammadovKamran <kamran.97.memmedov@gmail.com>
2022-09-26 09:02:22 +0400

    add SignIn and SignUp desktop version with redux toolkit

--- ecommerce_project/src/pages/LogIn/SignIn.jsx
+++ ecommerce_project/src/pages/LogIn/SignIn.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import loginStyle from "./login.module.css";
+import { Form, FormGroup, Input, Button, Label } from "reactstrap";
+import { useDispatch, useSelector } from "react-redux";
+import {  fetchUsers, selectAllUsers } from "../../redux/userSlice/userSlice";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/image/logo.png";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const allUsers = useSelector(selectAllUsers);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export default SignIn
+  const handleSubmit = (e) => {
+    if (email === "" && password === "") {
+      alert("Please enter email and password");
+    } else if (allUsers.find((user) => user.email === email && user.password === password)) {
+      alert("succesfull");
+      navigate("/");
+    } else {
+      alert("wrong email or password");
+    }
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+  return (
+    <div>
+      <div className={loginStyle.container}>
+        <div className={loginStyle.navbar}>
+          <div>
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+          </div>
+          <div className={loginStyle.navRightSide}>
+            <p>Not a member?</p>
+            <Link to="/signUp">Sign Up Now</Link>
+          </div>
+        </div>
+        <div className={loginStyle.main}>
+          <div className={loginStyle.main_leftSide_SignIn}>
+            <div>
+              <h3>We Offer the Best Products</h3>
+            </div>
+          </div>
+          <div className={loginStyle.main_rightSide}>
+            <div className={loginStyle.main_rightSide_form}>
+              <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                  <h3>Sign in to eTrade.</h3>
+                  <p>Enter your detail below</p>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="exampleEmail">Email</Label>
+                  <Input type="email" name="email" id="exampleEmail" placeholder="annie@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="examplePassword">Password</Label>
+                  <Input type="password" name="password" id="examplePassword" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </FormGroup>
+                <FormGroup className={loginStyle.signIn_forgotpassword_btn}>
+                  <Button type="submit" className={loginStyle.createAccount}>
+                    Sign In
+                  </Button>
+                  <Link to="">Forget Password?</Link>
+                </FormGroup>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- ecommerce_project/src/pages/LogIn/SignUp.jsx
+++ ecommerce_project/src/pages/LogIn/SignUp.jsx
@@ -3,10 +3,12 @@ import loginStyle from "./login.module.css";
 import { Form, FormGroup, Input, Button, Label } from "reactstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, fetchUsers, selectAllUsers } from "../../redux/userSlice/userSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/image/logo.png";
 
 const SignUp = () => {
-  const allUsers = useSelector(selectAllUsers);
+  const allUsers = useSelector(selectAllUsers)
+  const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
@@ -32,19 +34,47 @@ const SignUp = () => {
   return (
     <div>
       <div className={loginStyle.container}>
-        <div className={loginStyle.card}>
-          <Form onSubmit={handleSubmit}>
-            <FormGroup>
-              <Label for="exampleEmail">Email</Label>
-              <Input type="email" name="email" id="exampleEmail" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </FormGroup>
-            <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </FormGroup>
-
-            <Button type="submit">Sign Up</Button>
-          </Form>
+        <div className={loginStyle.navbar}>
+          <div>
+            <Link to="/">
+              
+              <img src={logo} alt="logo" />
+            </Link>
+          </div>
+          <div className={loginStyle.navRightSide}>
+            <p>Already a member?</p>
+            <Link to="/signIn">Sign In</Link>
+          </div>
+        </div>
+        <div className={loginStyle.main}>
+          <div className={loginStyle.main_leftSide}>
+            <div>
+              <h3>We Offer the Best Products</h3>
+            </div>
+          </div>
+          <div className={loginStyle.main_rightSide}>
+            <div className={loginStyle.main_rightSide_form}>
+              <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                  <h3>I'm New Here</h3>
+                  <p>Enter your detail below</p>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="exampleUserName">User Name</Label>
+                  <Input type="text" name="userName" id="userName" placeholder="anniemario" value={userName} onChange={(e) => setUserName(e.target.value)} />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="exampleEmail">Email</Label>
+                  <Input type="email" name="email" id="exampleEmail" placeholder="annie@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="examplePassword">Password</Label>
+                  <Input type="password" name="password" id="examplePassword" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </FormGroup>
+                <Button type="submit" className={loginStyle.createAccount}>Create Account</Button>
+              </Form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
